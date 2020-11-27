ef5173eddb9ec77e5ddbd51dc8cf9436ad7594d9
jiajia <jiajia@.com>
2020-11-27 18:08:58 +0800

    first commit

---
+++ src/main/java/HelloWorld.java
@@ -0,0 +1,10 @@
+/**
+ * @author Zoro_Gan
+ * @create 2020-11-27 下午 6:07
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("hello");
+        System.out.println("HelloWorld");
+    }
+}
