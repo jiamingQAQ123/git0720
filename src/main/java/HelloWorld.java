5940005ba44516a7f0eb2e229bc07ec293715580
jiajia <jiajia@.com>
2020-11-27 18:11:58 +0800

    hot-fix first commit

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -6,5 +6,7 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("hello");
         System.out.println("HelloWorld");
+        System.out.println("我是你爸爸，真伟大养你这么大");
+        System.out.println("low");
     }
 }
